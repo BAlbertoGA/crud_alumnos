8314ca95e68d2ff9b1ad6d790dcb2b54fa74e31e
BryanA <63431103+BAlbertoGA@users.noreply.github.com>
2026-01-02 07:16:40 -0600

    Implementacion de auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@ Route::resource('alumnos', AlumnoController::class);
 // Route::get('/escuelas', [EscuelaController::class, 'index'])->name('escuelas.index');
 // Route::get('/alumnos', [AlumnoController::class, 'index'])->name('alumnos.index');
 Route::get('/', [AlumnoController::class, 'index'])->name('home');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
