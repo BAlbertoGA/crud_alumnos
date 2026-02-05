282aa6b37f8251eec96f000ad4677e3c7d3a1fe1
BryanA <63431103+BAlbertoGA@users.noreply.github.com>
2026-02-05 13:20:34 -0600

    ultima actualizacion

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,12 @@ use App\Http\Controllers\AlumnoController;
 //     return view('alumnos.index');
 // });
 
-Route::resource('escuelas', EscuelaController::class);
-Route::resource('alumnos', AlumnoController::class);
+Route::resource('escuelas', EscuelaController::class)->middleware('auth');
+Route::resource('alumnos', AlumnoController::class)->middleware('auth');
 
 // Route::get('/escuelas', [EscuelaController::class, 'index'])->name('escuelas.index');
 // Route::get('/alumnos', [AlumnoController::class, 'index'])->name('alumnos.index');
-Route::get('/', [AlumnoController::class, 'index'])->name('home');
+Route::get('/', [AlumnoController::class, 'index'])->name('home')->middleware('auth');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
